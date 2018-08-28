8c31e0550739033884ce8475d5d1c9dd183deab4
Meinolf Droste <mdwp.post@gmail.com>
2018-08-28 17:11:55 +0200

    thumbnail fix

--- src/Objs/Redirect/RedirectEditingConfig.js
+++ src/Objs/Redirect/RedirectEditingConfig.js
@@ -2,27 +2,27 @@ import * as Scrivito from 'scrivito';
 import redirectObjIcon from '../../assets/images/redirect_obj.svg';
 
 Scrivito.provideEditingConfig('Redirect', {
-  title: 'Redirect',
-  thumbnail: `/${redirectObjIcon}`,
-  hideInSelectionDialogs: true,
-  attributes: {
-    link: {
-      title: 'Link',
+    title: 'Redirect',
+    thumbnail: redirectObjIcon,
+    hideInSelectionDialogs: true,
+    attributes: {
+        link: {
+            title: 'Link',
+        },
+        openInUi: {
+            title: 'Open in Scrivito UI?',
+            description: 'Default: No',
+            values: [
+                { value: 'yes', title: 'Yes' },
+                { value: 'no', title: 'No' },
+            ],
+        },
     },
-    openInUi: {
-      title: 'Open in Scrivito UI?',
-      description: 'Default: No',
-      values: [
-        { value: 'yes', title: 'Yes' },
-        { value: 'no', title: 'No' },
-      ],
+    properties: [
+        'link',
+        'openInUi',
+    ],
+    initialContent: {
+        openInUi: 'no',
     },
-  },
-  properties: [
-    'link',
-    'openInUi',
-  ],
-  initialContent: {
-    openInUi: 'no',
-  },
 });
